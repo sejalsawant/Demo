23ea7ca133fae161c2750714b4420e76525bb488
sejal <sawantsejal88@gmail.com>
2021-11-10 19:19:48 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/APITest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+	
+	@Test
+	
+	public void PostJira()
+	{
+		System.out.println("PostJira");
+	}
+	
+	@Test
+	public void DeleteTwitter()
+	{
+		System.out.println("DeleteTwitter");
+	}
+}
